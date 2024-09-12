992d4147c26b0fbca651b8bfb9821012c4d7502e
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2024-09-12 21:50:26 +0100

    fix: remove db-related attributes from domain models

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -5,17 +5,12 @@ namespace Domain.Entities;
 public class User(Guid Id, string firstName, string lastName, string email, string phone, DateTime dateOfBirth) : Entity(Id)
 {
 
-    [MaxLength(255)]
     public string FirstName { get; private set; } = firstName;
 
-    [MaxLength(255)]
     public string LastName { get; private set; } = lastName;
 
-    [EmailAddress]
-    [MaxLength(255)]
     public string Email { get; private set; } = email;
 
-    [MaxLength(255)]
     public string Phone { get; private set; } = phone;
 
     public DateTimeOffset DateOfBirth { get; } = dateOfBirth;
